1f2f8d67e9b2c4166dc909692c15c769b1a74898
amo935 <aguedambenya@gmail.com>
2026-09-02 15:47:14 +0200

    Add files via upload

---
+++ Simple examples/If-else simple.py
@@ -0,0 +1,12 @@
+# Asign to `system` one especific operative sistem 
+# This variable represents the operative sistem that it's running
+
+system = "0S 3"
+
+# If OS 2 is running, display a message saying 'no update needed'
+# Otherwise, display a message saying 'update needed'
+
+if system == "OS 2":
+    print("no update needed")
+else:
+    print("update needed")
